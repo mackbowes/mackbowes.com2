a4189e5d4d74568aeef80d0288a76c5948bc689b
Mack Bowes <mackenziepbowes@gmail.com>
2022-01-17 13:55:14 -0800

    added more links

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Box, Heading, Text, Image } from "@chakra-ui/react";
+import { Box, Heading, Text, Image, Link } from "@chakra-ui/react";
+import toast from "react-hot-toast";
 
 export default function Home() {
   return (
@@ -37,7 +38,37 @@ export default function Home() {
           React | Node | Solidity | Tokenomics | Audits | Shilling
         </Heading>
         <Text sx={{ margin: `1rem 0` }}>
-          Member of Raidguild, Shillshop, and Quiver DAOs.
+          Member of{" "}
+          <Link
+            href="https://app.daohaus.club/dao/0x64/0xfe1084bc16427e5eb7f13fc19bcd4e641f7d571f"
+            target="_blank"
+            rel="noreferrer"
+            sx={{ fontWeight: `700`, transition: `all 0.25s` }}
+            _hover={{ color: `#1089CC` }}
+          >
+            Raidguild
+          </Link>
+          ,{" "}
+          <Link
+            href="https://app.daohaus.club/dao/0x64/0x50d9fbebdd7fd36c0716fc1faaa9248bab6bbcc0"
+            target="_blank"
+            rel="noreferrer"
+            sx={{ fontWeight: `700`, transition: `all 0.25s` }}
+            _hover={{ color: `#1089CC` }}
+          >
+            Shillshop
+          </Link>
+          , and{" "}
+          <Link
+            href="https://app.daohaus.club/dao/0x64/0xd18120b22e7c26cda11f5194faea4d9da53722fd"
+            target="_blank"
+            rel="noreferrer"
+            sx={{ fontWeight: `700`, transition: `all 0.25s` }}
+            _hover={{ color: `#1089CC` }}
+          >
+            Quiver
+          </Link>{" "}
+          DAOs.
           <br />
           Currently working on SW DAO, STOA, METL, The Green Pill, and ShillShop
           Podcast/Twitter Spaces.
@@ -96,6 +127,59 @@ export default function Home() {
               Github →
             </Text>
           </a>
+          <a href="https://www.raidguild.org/" target="_blank" rel="noreferrer">
+            <Text
+              sx={{
+                fontSize: `1rem`,
+                border: `2px solid #F9E6E6`,
+                borderRadius: `2rem`,
+                padding: `.5rem 3rem`,
+                display: `inline-block`,
+                transition: `all 0.25s`,
+              }}
+              _hover={{
+                backgroundColor: `#F9E6E6`,
+                cursor: `pointer`,
+                color: `#CC252C`,
+              }}
+            >
+              Raidguild →
+            </Text>
+          </a>
+          <Text
+            sx={{
+              fontSize: `1rem`,
+              border: `2px solid #F9E6E6`,
+              borderRadius: `2rem`,
+              padding: `.5rem 3rem`,
+              display: `inline-block`,
+              transition: `all 0.25s`,
+            }}
+            _hover={{
+              backgroundColor: `#F9E6E6`,
+              cursor: `pointer`,
+              color: `#CC252C`,
+            }}
+            onClick={() => {
+              navigator.clipboard.writeText(
+                "0xBc4A2b0B65e39bAE9bedad1798B824EAf0A60639"
+              );
+              toast.success("Address Copied to Clipboard", {
+                style: {
+                  border: "1px solid #CC252C",
+                  padding: "16px",
+                  color: "#CC252C",
+                  backgroundColor: `#F9E6E6`,
+                },
+                iconTheme: {
+                  primary: "#CC252C",
+                  secondary: "#FFFAEE",
+                },
+              });
+            }}
+          >
+            Wallet →
+          </Text>
         </Box>
       </Box>
     </Box>
