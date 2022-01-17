5adc3cc4bf790feb08211fde132aad35e940b9ec
Mack Bowes <mackenziepbowes@gmail.com>
2022-01-17 14:09:33 -0800

    pushy pushy

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ export default function Home() {
           position: `relative`,
           userSelect: `none`,
           overflowY: `hidden`,
-          mixBlendMode: ``,
         }}
       >
         <Box>
